bba12efd2ab09d413fffdd215d96c4645b0c3f32
Zihan Zhou <35605731+zzihan1206@users.noreply.github.com>
2018-04-17 17:00:21 -0600

    Update geometry.h

--- geometry.h
+++ geometry.h
@@ -24,3 +24,5 @@ bool coord_2d_eq(const coord_2d_t* a, const coord_2d_t* b);
 
 /* Calculate the midpoint between two 2D coordinates and load into mid */
 void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b);
+/* calc area */
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c);
